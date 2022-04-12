4e7756b5b1812b5ac03bbd17e7e5c32e9cc9a725
Héctor Moreno <hkmoca@gmail.com>
2022-04-12 06:32:08 -0700

    Add object

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Héctor Moreno Cano on 12/04/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
