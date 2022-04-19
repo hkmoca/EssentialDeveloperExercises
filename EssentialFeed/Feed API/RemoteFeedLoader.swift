c57e22a8fe36be239bb0e9cb27fa5be4e3ff1414
Héctor Moreno <hkmoca@gmail.com>
2022-04-19 06:20:11 -0700

    Add test

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -11,6 +11,7 @@ public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
     
+    
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
